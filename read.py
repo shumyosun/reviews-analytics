d584ef9af620df3c9966b128731d9b6067dc1f16
Myo Crystal <shujou.sun@gmail.com>
2022-08-28 19:03:37 +0800

    add word count

--- read.py
+++ read.py
@@ -7,12 +7,12 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print('檔案讀取完了, 總共有', len(data), '筆資料')
+print(data[0])
 
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
-	print('留言的平均長度為', sum_len/len(data))
-
+print('留言的平均長度為', sum_len/len(data))
 
 new = []
 for d in data:
@@ -22,11 +22,35 @@ print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
 
-
-good =[]
+good = []
 for d in data:
 	if 'good' in d:
 		good.append(d)
 print('一共有', len(good), '筆留言提到good')
-print(good[0])
 
+wc = {}
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為: ', wc[word])
+	else:
+		print('這個字沒有出現過喔!')
+
+print('感謝使用本次功能')
